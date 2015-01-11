f5ec93391b3fad3e56539b47a5a7ec587db1cc18
Flavio Tulino <flavio.tulino@gmail.com>
2015-01-11 17:45:07 +0100

    added route

--- controllers/app.js
+++ controllers/app.js
@@ -1,17 +1,22 @@
 var me = angular.module('me-app', [
-  'ngRoute',
-  'HomeController'
+    'ngRoute',
+    'HomeController'
 ]);
 
 
-me.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/', {
-        templateUrl: 'templates/home.html',
-        controller: 'HomeController'
-      }).
-      otherwise({
-        redirectTo: '/'
-      });
-  }]);
+me.config(['$routeProvider', '$locationProvider',
+    function ($routeProvider, $locationProvider) {
+        $locationProvider.html5Mode(true);
+
+        $routeProvider.
+                when('/', {
+                    templateUrl: 'templates/home.html',
+                    controller: 'HomeController'
+                }).
+                when("/test", {
+                    templateUrl: "templates/test.html"
+                }).
+                otherwise({
+                    redirectTo: '/'
+                });
+    }]);
